db4ede74a9799a44182aaf13c4ad58c7790f70fe
mooncantara <fxcdsxcd@gmail.com>
2025-08-30 10:48:33 +0300

    Обновил TaskItem

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,12 @@
 import { observer } from "mobx-react-lite";
 import { taskStore } from "../store/TaskStore";
+import TaskItem from "./TaskItem";
 
 const TaskList = observer(function TaskList() {
   return (
     <ul style={{ paddingLeft: 16 }}>
       {taskStore.tasks.map((t) => (
-        <li key={t.id}>{t.title}</li>
+        <TaskItem key={t.id} task={t} />
       ))}
     </ul>
   );
