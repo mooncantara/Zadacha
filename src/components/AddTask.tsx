30f88293430538921e04849f1e45745413d721f0
mooncantara <fxcdsxcd@gmail.com>
2025-08-30 10:37:01 +0300

    Добавил AddTask

---
+++ src/components/AddTask.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { taskStore } from "../store/TaskStore";
+
+export default function AddTask() {
+  const [title, setTitle] = useState("");
+
+  const onAdd = () => {
+    const trimmed = title.trim();
+    if (!trimmed) return;
+    taskStore.addTask(null, trimmed); // добавляем в корень
+    setTitle(""); // очистка поля
+  };
+
+  return (
+    <div style={{ display: "flex", gap: 8 }}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Новая задача"
+      />
+      <button onClick={onAdd}>Добавить</button>
+    </div>
+  );
+}
+
